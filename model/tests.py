114586617bfcd7b4e347e40a80f3b99d402a0b4d
ailsonazevedo <61508532+ailsonazevedo@users.noreply.github.com>
2022-06-13 10:51:06 -0300

    test: update tests.py

--- model/tests.py
+++ model/tests.py
@@ -5,10 +5,10 @@ class TestBiblioteca(TestCase):
     #verificar livro ja emprestado
     def test_verificar_livro_ja_emprestado(self):
         livro1 = Livro(1, 'livro1')
-        empresto1 = Emprestimo(Usuario('usuario1', '00000000000'), livro1)
+        empresto1 = Emprestimo(Usuario('usuario1', '06532785350'), livro1)
 
         livro2 = Livro(2, 'livro2')
-        empresto2 = Emprestimo(Usuario('usuario2', '00000000001'), livro2)
+        empresto2 = Emprestimo(Usuario('usuario2', '12332785370'), livro2)
 
         biblioteca = Biblioteca()
         biblioteca.livros = [livro1, livro2]
@@ -35,10 +35,10 @@ class TestBiblioteca(TestCase):
 
         biblioteca = Biblioteca()
         biblioteca.livros = [livro1, livro2, livro3]
-        usuario1 = Usuario('usuario1', '00000000002')
+        usuario1 = Usuario('usuario1', '14000000002')
         livros_usuario1 = [livro1, livro2]
 
-        funcionario1 = Funcionario('funcionario1', '00000000040')
+        funcionario1 = Funcionario('funcionario1', '56700000040')
         biblioteca.funcionarios = [funcionario1]
         biblioteca.realizarEmprestimo(funcionario1, livros_usuario1, usuario1)
 
@@ -53,8 +53,8 @@ class TestBiblioteca(TestCase):
         livro5 = Livro(5, 'livro5')
         livro6 = Livro(6, 'livro6')
 
-        usuario1 = Usuario('usuario1', '00000000002')
-        usuario2 = Usuario('usuario2', '00000000004')
+        usuario1 = Usuario('usuario1', '14000000002')
+        usuario2 = Usuario('usuario2', '08691600004')
 
         biblioteca = Biblioteca()
         biblioteca.livros = [livro1, livro2, livro3, livro4, livro5, livro6]
@@ -69,8 +69,8 @@ class TestBiblioteca(TestCase):
     #solicitar emprestimo com funcionario invalido
     def test_solicitar_emprestimo_com_funcionario_invalido(self):
         livro1 = Livro(1, 'livro1')
-        usuario1 = Usuario('usuario1', '00000000002')
-        funcionario1 = Funcionario('funcionario1', '00000000003')
+        usuario1 = Usuario('usuario1', '14000000002')
+        funcionario1 = Funcionario('funcionario1', '12300000003')
 
         biblioteca = Biblioteca()
         biblioteca.livros = [livro1]
